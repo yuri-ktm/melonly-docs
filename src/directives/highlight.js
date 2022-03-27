15e67430485739b727800a4920692194d0cc7031
Doc077 <dom.rajkowski@gmail.com>
2022-03-27 15:57:43 +0200

    Improve syntax highlighting

---
+++ src/directives/highlight.js
@@ -0,0 +1,76 @@
+const keywords = [
+  'constructor',
+  'each',
+  'from',
+  'import',
+  'in',
+  'melon',
+  'npm',
+  'private',
+  'protected',
+  'public',
+  'return',
+  'this',
+]
+
+const types = [
+  'number',
+  'string',
+  'ViewResponse',
+]
+
+const additionals = [
+  '-g',
+  '-v',
+  '--version',
+]
+
+const comments = [
+  /(\/\/.*?)/gm,
+  /(\/\* ?.*? ?\*\/)/gm,
+  /(# ?.*?)/gm,
+]
+
+const signs = ['.', '@', ':', '(', ')', '[', ']', '{', '}']
+
+const literals = [
+  /(')(.*?)'/gm,
+  /(`)(.*?)`/gm,
+]
+
+export default {
+  mounted(el) {
+    let content = el.innerHTML
+
+    for (const keyword of keywords) {
+      content = content.replaceAll(keyword, `<span class="text-pink-400">${keyword}</span>`)
+    }
+
+    for (const additional of additionals) {
+      content = content.replaceAll(additional, `<span class="opacity-80">${additional}</span>`)
+    }
+
+    for (const comment of comments) {
+      content = content.replaceAll(comment, `<span class="opacity-70 italic">$1</span>`)
+    }
+
+    for (const sign of signs) {
+      content = content.replaceAll(sign, `<span class="text-gray-200">${sign}</span>`)
+    }
+
+    for (const type of types) {
+      content = content.replaceAll(type, `<span class="text-purple-300">${type}</span>`)
+    }
+
+    /**
+     * Literals
+     */
+    for (const literal of literals) {
+      for (const expression of content.matchAll(literal) ?? []) {
+        content = content.replaceAll(expression[0], `<span class="text-sky-300">${expression[1]}${expression[2]}${expression[1]}</span>`)
+      }
+    }
+
+    el.innerHTML = content
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import { createApp } from 'vue'
 import { router } from './routing/router'
+
+import highlightDirective from './directives/highlight'
 import outclickDirective from './directives/outclick'
+
 import BaseSelect from './components/BaseSelect.vue'
 import BaseSwitch from './components/BaseSwitch.vue'
 import App from './App.vue'
+
 import './styles/main.css'
 
 const app = createApp(App)
 
 app.use(router)
 
+app.directive('highlight', highlightDirective)
 app.directive('outclick', outclickDirective)
+
 app.component('BaseSelect', BaseSelect)
 app.component('BaseSwitch', BaseSwitch)
 
