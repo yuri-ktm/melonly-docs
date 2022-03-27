0c3d3cab550b46cc530fc136e8e238f3a0e31af5
Doc077 <dom.rajkowski@gmail.com>
2022-03-27 17:28:41 +0200

    Update home page

--- src/components/CodeBlock.vue
+++ src/components/CodeBlock.vue
@@ -1,3 +1,11 @@
 <template>
-  <pre class="bg-gray-900 text-slate-300 text-sm leading-8 whitespace-pre overflow-hidden rounded-xl shadow-lg px-5 md:px-7 py-3 md:py-5" v-highlight><slot></slot></pre>
+  <div class="relative group bg-gray-900 rounded-xl px-5 md:px-7 py-3 md:py-5 shadow-lg">
+    <pre class="text-slate-300 text-sm leading-8 whitespace-pre overflow-hidden" v-highlight><slot></slot></pre>
+
+    <div class="absolute right-3 top-3 opacity-70 cursor-pointer hidden group-hover:block rounded-md bg-gray-900 p-1" title="Copy to clipboard">
+      <svg xmlns="http://www.w3.org/2000/svg" class="stroke-gray-50 w-6 h-6" fill="none" viewBox="0 0 24 24" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+      </svg>
+    </div>
+  </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="flex flex-col justify-center items-center w-full h-[calc(100vh-14rem)] p-5 md:py-32 md:px-48">
-    <h1 class="text-4xl font-medium font-nunito md:text-5xl tracking-wide mb-10">
+  <section class="flex flex-col justify-center items-center w-full h-[calc(100vh-17rem)] dark:bg-gray-800/60 p-5 md:py-32 md:px-48">
+    <h1 class="text-4xl font-bold font-nunito md:text-5xl tracking-wide mb-10">
       The <span class="text-emerald-500 dark:text-emerald-400">modern</span> Node.js framework
     </h1>
 
@@ -16,7 +16,7 @@
       </RouterLink>
 
       <a
-        class="bg-slate-100 inline-block cursor-pointer text-base px-4 md:px-6 py-3 rounded-xl text-gray-500 transition-all duration-300 hover:opacity-90 hover:-translate-y-1 select-none ml-2 dark:bg-gray-800 dark:text-gray-50 outline-none focus:ring-2 ring-offset-2 ring-offset-white dark:ring-offset-gray-900 focus:ring-emerald-400"
+        class="bg-slate-100 inline-block cursor-pointer text-base px-4 md:px-6 py-3 rounded-xl text-gray-500 transition-all duration-300 hover:opacity-90 hover:-translate-y-1 select-none ml-2 dark:bg-gray-700/80 dark:text-gray-50 outline-none focus:ring-2 ring-offset-2 ring-offset-white dark:ring-offset-gray-900 focus:ring-emerald-400"
         target="_blank"
         href="https://github.com/Doc077/melonly"
       >
@@ -27,59 +27,57 @@
       </a>
     </div>
 
-    <CodeBlock class="dark:bg-gray-800"><span class="select-none mr-3">$</span>npm install -g @melonly/cli</CodeBlock>
+    <CodeBlock><span class="select-none mr-3">$</span>npm install -g @melonly/cli</CodeBlock>
   </section>
 
-  <section class="flex flex-col justify-center items-center w-full h-[calc(100vh-14rem)] bg-gray-900 dark:bg-gray-800/80 text-gray-50 p-6 md:py-20 md:px-48">
-    <h2 class="text-3xl md:text-4xl font-nunito mb-8"><span class="text-emerald-400">Awesome</span> developer experience</h2>
+  <section class="flex flex-col justify-center items-center w-full h-[calc(100vh-12rem)] bg-gray-900 text-gray-50 p-6 md:py-20 md:px-48">
+    <h2 class="text-3xl font-nunito mb-8"><span class="text-emerald-400">Awesome</span> developer experience</h2>
 
     <p class="opacity-90 text-lg mb-10">Melonly focuses on elegant and simple syntax which scales well in larger projects.</p>
 
-    <CodeBlock class="w-full bg-gray-800 dark:bg-gray-900 w-auto">
+    <CodeBlock class="w-full bg-gray-800/70 w-auto">
       @Get('/')<br>
       public index(): ViewResponse {<br>
       &nbsp;&nbsp;return this.response.render('home', { message: 'Hello World' })<br>
       }
     </CodeBlock>
 
-    <CodeBlock class="w-full bg-gray-800 dark:bg-gray-900 w-auto mt-7">
-      [if authenticated]<br>
-      &nbsp;&nbsp;&lt;div class="welcome"&gt;&#123;&#123; message &#125;&#125;&lt;/div&gt;<br>
-      [else]<br>
-      &nbsp;&nbsp;&lt;a href="/login"&gt;Log in&lt;/a&gt;<br>
+    <CodeBlock class="w-full bg-gray-800/70 w-auto mt-7">
+      [if userLogged]<br>
+      &nbsp;&nbsp;&lt;div class="welcome"&gt;&#123;&#123; message &#125;&#125; &#123;&#123; username &#125;&#125;&lt;/div&gt;<br>
       [/if]
     </CodeBlock>
   </section>
 
-  <section class="flex flex-col justify-center items-center w-full h-[calc(100vh-24rem)] p-6 md:py-20 md:px-48">
-    <h2 class="text-3xl md:text-4xl font-nunito mb-12"><span class="text-emerald-500 dark:text-emerald-400">Why</span> should you try Melonly?</h2>
+  <section class="flex flex-col justify-center items-center w-full h-[calc(100vh-24rem)] dark:bg-gray-800/60 p-6 md:py-20 md:px-48">
+    <h2 class="text-3xl font-nunito mb-12"><span class="text-emerald-500 dark:text-emerald-400">Why</span> should you try Melonly?</h2>
 
-    <div class="bg-slate-100 dark:bg-gray-800 flex items-center rounded-lg p-5 mb-5">
+    <div class="bg-slate-100 dark:bg-gray-900 flex items-center rounded-lg p-5 mb-5">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-emerald-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
       </svg>
 
-      <span class="opacity-90">Performant</span>
+      <span class="opacity-90">Super fast</span>
     </div>
 
-    <div class="bg-slate-100 dark:bg-gray-800 flex items-center rounded-lg p-5 mb-5">
+    <div class="bg-slate-100 dark:bg-gray-900 flex items-center rounded-lg p-5 mb-5">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-emerald-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
       </svg>
 
-      <span class="opacity-90">Auth system, routing & more</span>
+      <span class="opacity-90">Routing, authentication features & more</span>
     </div>
 
-    <div class="bg-slate-100 dark:bg-gray-800 flex items-center rounded-lg p-5 mb-5">
+    <div class="bg-slate-100 dark:bg-gray-900 flex items-center rounded-lg p-5 mb-5">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-emerald-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
       </svg>
 
-      <span class="opacity-90">Secure web apps</span>
+      <span class="opacity-90">Secure & scalable</span>
     </div>
   </section>
 
-  <footer class="flex flex-col justify-center items-center w-full h-96 bg-gray-900 dark:bg-gray-800/60 text-center text-gray-50 p-3 md:py-32 mt-32">
+  <footer class="flex flex-col justify-center items-center w-full h-96 bg-gray-900 dark:bg-gray-900 text-center text-gray-50 p-3 md:py-32">
     <p class="opacity-80 mb-3">Copyright © 2022 Dominik Rajkowski</p>
 
     <p class="opacity-80">Melonly.js open-source framework is licensed under <a class="text-emerald-500 dark:text-emerald-400 hover:border-b border-emerald-500" href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a>.</p>
